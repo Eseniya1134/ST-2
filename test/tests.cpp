561036eb332ce6047e77c31c05af336cf1d8fea6
Eseniya1134 <shakirova.esya23@mail.ru>
2026-03-15 08:14:40 +0300

    ..

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,10 +1,11 @@
 // Copyright 2025 UNN-CS Team
 
+#include "circle.h"
+#include "tasks.h"
+
 #include <gtest/gtest.h>
 #include <cstdint>
-#include "circle.h"
 #include <cmath>
-#include "tasks.h"
 
 const double EPS = 1e-6;
 
